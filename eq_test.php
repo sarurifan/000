5fa1666ee41c9ab70dc8f6dce2a0eef782dfab77
saruri <35908306+sarurifan@users.noreply.github.com>
2018-02-10 11:49:20 +0800

    00
    0

---
+++ eq_test.php
@@ -0,0 +1,361 @@
+<?php
+
+	$sarr['code'] = 200;
+	$sarr['title'] = 'eq题库';
+
+	$sarr['data']['01']['title'] = '我有能力克服各种困难：';
+	$sarr['data']['01']['list'][0]['question'] = '是的';
+	$sarr['data']['01']['list'][1]['question'] = '不一定';
+	$sarr['data']['01']['list'][2]['question'] = '不是的';
+	$sarr['data']['01']['list'][0]['score'] = '6';
+	$sarr['data']['01']['list'][1]['score'] = '3';
+	$sarr['data']['01']['list'][2]['score'] = '0';
+	$sarr['data']['01']['list'][0]['type'] = 'radio';
+	$sarr['data']['01']['list'][1]['type'] = 'radio';
+	$sarr['data']['01']['list'][2]['type'] = 'radio';
+
+	$sarr['data']['02']['title'] = '如果我能到一个新的环境，我要把生活安排得：';
+	$sarr['data']['02']['list'][0]['question'] = '和从前相仿';
+	$sarr['data']['02']['list'][1]['question'] = '不一定';
+	$sarr['data']['02']['list'][2]['question'] = '和从前不一样的';
+	$sarr['data']['02']['list'][0]['score'] = '6';
+	$sarr['data']['02']['list'][1]['score'] = '3';
+	$sarr['data']['02']['list'][2]['score'] = '0';
+	$sarr['data']['02']['list'][0]['type'] = 'radio';
+	$sarr['data']['02']['list'][1]['type'] = 'radio';
+	$sarr['data']['02']['list'][2]['type'] = 'radio';
+
+	$sarr['data']['03']['title'] = '一生中，我觉得自已能达到我所预想的目标：';
+	$sarr['data']['03']['list'][0]['question'] = '是的';
+	$sarr['data']['03']['list'][1]['question'] = '不一定';
+	$sarr['data']['03']['list'][2]['question'] = '不是的';
+	$sarr['data']['03']['list'][0]['score'] = '6';
+	$sarr['data']['03']['list'][1]['score'] = '3';
+	$sarr['data']['03']['list'][2]['score'] = '0';
+	$sarr['data']['03']['list'][0]['type'] = 'radio';
+	$sarr['data']['03']['list'][1]['type'] = 'radio';
+	$sarr['data']['03']['list'][2]['type'] = 'radio';
+
+	$sarr['data']['04']['title'] = '不知为什么，有些人总是回避或冷淡我：';
+	$sarr['data']['04']['list'][0]['question'] = '不是的';
+	$sarr['data']['04']['list'][1]['question'] = '不一定';
+	$sarr['data']['04']['list'][2]['question'] = '是的';
+	$sarr['data']['04']['list'][0]['score'] = '6';
+	$sarr['data']['04']['list'][1]['score'] = '3';
+	$sarr['data']['04']['list'][2]['score'] = '0';
+	$sarr['data']['04']['list'][0]['type'] = 'radio';
+	$sarr['data']['04']['list'][1]['type'] = 'radio';
+	$sarr['data']['04']['list'][2]['type'] = 'radio';
+
+	$sarr['data']['05']['title'] = '在大街上，我常常避开我不愿打招呼的人：';
+	$sarr['data']['05']['list'][0]['question'] = '从未如此';
+	$sarr['data']['05']['list'][1]['question'] = '偶然如此';
+	$sarr['data']['05']['list'][2]['question'] = '有时如此';
+	$sarr['data']['05']['list'][0]['score'] = '6';
+	$sarr['data']['05']['list'][1]['score'] = '3';
+	$sarr['data']['05']['list'][2]['score'] = '0';
+	$sarr['data']['05']['list'][0]['type'] = 'radio';
+	$sarr['data']['05']['list'][1]['type'] = 'radio';
+	$sarr['data']['05']['list'][2]['type'] = 'radio';
+
+ 	$sarr['data']['05']['title'] = '在大街上，我常常避开我不愿打招呼的人：';
+	$sarr['data']['05']['list'][0]['question'] = '从未如此';
+	$sarr['data']['05']['list'][1]['question'] = '偶然如此';
+	$sarr['data']['05']['list'][2]['question'] = '有时如此';
+	$sarr['data']['05']['list'][0]['score'] = '6';
+	$sarr['data']['05']['list'][1]['score'] = '3';
+	$sarr['data']['05']['list'][2]['score'] = '0';
+	$sarr['data']['05']['list'][0]['type'] = 'radio';
+	$sarr['data']['05']['list'][1]['type'] = 'radio';
+	$sarr['data']['05']['list'][2]['type'] = 'radio';
+
+	$sarr['data']['06']['title'] = '当我集中精力工作时，假使有人在旁边高谈阔论：';
+	$sarr['data']['06']['list'][0]['question'] = '我仍能用心工作';
+	$sarr['data']['06']['list'][1]['question'] = '介于A、C之间';
+	$sarr['data']['06']['list'][2]['question'] = '我不能专心且感到愤怒';
+	$sarr['data']['06']['list'][0]['score'] = '6';
+	$sarr['data']['06']['list'][1]['score'] = '3';
+	$sarr['data']['06']['list'][2]['score'] = '0';
+	$sarr['data']['06']['list'][0]['type'] = 'radio';
+	$sarr['data']['06']['list'][1]['type'] = 'radio';
+	$sarr['data']['06']['list'][2]['type'] = 'radio';
+
+	$sarr['data']['07']['title'] = '我不论到什么地方，都能清晰地辨别方向：';
+	$sarr['data']['07']['list'][0]['question'] = '是的';
+	$sarr['data']['07']['list'][1]['question'] = '不一定';
+	$sarr['data']['07']['list'][2]['question'] = '不是的';
+	$sarr['data']['07']['list'][0]['score'] = '6';
+	$sarr['data']['07']['list'][1]['score'] = '3';
+	$sarr['data']['07']['list'][2]['score'] = '0';
+	$sarr['data']['07']['list'][0]['type'] = 'radio';
+	$sarr['data']['07']['list'][1]['type'] = 'radio';
+	$sarr['data']['07']['list'][2]['type'] = 'radio';
+
+	$sarr['data']['08']['title'] = '我热爱所学的专业和所从事的工作：';
+	$sarr['data']['08']['list'][0]['question'] = '是的';
+	$sarr['data']['08']['list'][1]['question'] = '不一定';
+	$sarr['data']['08']['list'][2]['question'] = '不是的';
+	$sarr['data']['08']['list'][0]['score'] = '6';
+	$sarr['data']['08']['list'][1]['score'] = '3';
+	$sarr['data']['08']['list'][2]['score'] = '0';
+	$sarr['data']['08']['list'][0]['type'] = 'radio';
+	$sarr['data']['08']['list'][1]['type'] = 'radio';
+	$sarr['data']['08']['list'][2]['type'] = 'radio';
+
+	$sarr['data']['09']['title'] = '气候的变化不会影响我的情绪：';
+	$sarr['data']['09']['list'][0]['question'] = '是的';
+	$sarr['data']['09']['list'][1]['question'] = '介于A、C之间';
+	$sarr['data']['09']['list'][2]['question'] = '不是的';
+	$sarr['data']['09']['list'][0]['score'] = '6';
+	$sarr['data']['09']['list'][1]['score'] = '3';
+	$sarr['data']['09']['list'][2]['score'] = '0';
+	$sarr['data']['09']['list'][0]['type'] = 'radio';
+	$sarr['data']['09']['list'][1]['type'] = 'radio';
+	$sarr['data']['09']['list'][2]['type'] = 'radio';
+
+	$sarr['data']['10']['title'] = '我从不因流言蜚语而气愤：';
+	$sarr['data']['10']['list'][0]['question'] = '是的';
+	$sarr['data']['10']['list'][1]['question'] = '介于A、C之间';
+	$sarr['data']['10']['list'][2]['question'] = '不是的';
+	$sarr['data']['10']['list'][0]['score'] = '5';
+	$sarr['data']['10']['list'][1]['score'] = '2';
+	$sarr['data']['10']['list'][2]['score'] = '0';
+	$sarr['data']['10']['list'][0]['type'] = 'radio';
+	$sarr['data']['10']['list'][1]['type'] = 'radio';
+	$sarr['data']['10']['list'][2]['type'] = 'radio';
+
+	$sarr['data']['11']['title'] = '我善于控制自己的面部表情：';
+	$sarr['data']['11']['list'][0]['question'] = '是的';
+	$sarr['data']['11']['list'][1]['question'] = '不太确定';
+	$sarr['data']['11']['list'][2]['question'] = '不是的';
+	$sarr['data']['11']['list'][0]['score'] = '5';
+	$sarr['data']['11']['list'][1]['score'] = '2';
+	$sarr['data']['11']['list'][2]['score'] = '0';
+	$sarr['data']['11']['list'][0]['type'] = 'radio';
+	$sarr['data']['11']['list'][1]['type'] = 'radio';
+	$sarr['data']['11']['list'][2]['type'] = 'radio';
+
+	$sarr['data']['12']['title'] = '在就寝时，我常常：';
+	$sarr['data']['12']['list'][0]['question'] = '极易入睡';
+	$sarr['data']['12']['list'][1]['question'] = '介于A、C之间';
+	$sarr['data']['12']['list'][2]['question'] = '不易入睡';
+	$sarr['data']['12']['list'][0]['score'] = '5';
+	$sarr['data']['12']['list'][1]['score'] = '2';
+	$sarr['data']['12']['list'][2]['score'] = '0';
+	$sarr['data']['12']['list'][0]['type'] = 'radio';
+	$sarr['data']['12']['list'][1]['type'] = 'radio';
+	$sarr['data']['12']['list'][2]['type'] = 'radio';
+
+	$sarr['data']['13']['title'] = '有人侵扰我时，我：';
+	$sarr['data']['13']['list'][0]['question'] = '不露声色';
+	$sarr['data']['13']['list'][1]['question'] = '介于A、C之间';
+	$sarr['data']['13']['list'][2]['question'] = '大声抗议，以泄己愤';
+	$sarr['data']['13']['list'][0]['score'] = '5';
+	$sarr['data']['13']['list'][1]['score'] = '2';
+	$sarr['data']['13']['list'][2]['score'] = '0';
+	$sarr['data']['13']['list'][0]['type'] = 'radio';
+	$sarr['data']['13']['list'][1]['type'] = 'radio';
+	$sarr['data']['13']['list'][2]['type'] = 'radio';
+
+	$sarr['data']['14']['title'] = '在和人争辨或工作出现失误后，我常常感到震颤，精疲力竭，而不能继承安心工作：';
+	$sarr['data']['14']['list'][0]['question'] = '不是的';
+	$sarr['data']['14']['list'][1]['question'] = '介于A、C之间';
+	$sarr['data']['14']['list'][2]['question'] = '是的';
+	$sarr['data']['14']['list'][0]['score'] = '5';
+	$sarr['data']['14']['list'][1]['score'] = '2';
+	$sarr['data']['14']['list'][2]['score'] = '0';
+	$sarr['data']['14']['list'][0]['type'] = 'radio';
+	$sarr['data']['14']['list'][1]['type'] = 'radio';
+	$sarr['data']['14']['list'][2]['type'] = 'radio';
+
+	$sarr['data']['15']['title'] = '我常常被一些无谓的小事困扰：';
+	$sarr['data']['15']['list'][0]['question'] = '不是的';
+	$sarr['data']['15']['list'][1]['question'] = '介于A、C之间';
+	$sarr['data']['15']['list'][2]['question'] = '是的';
+	$sarr['data']['15']['list'][0]['score'] = '5';
+	$sarr['data']['15']['list'][1]['score'] = '2';
+	$sarr['data']['15']['list'][2]['score'] = '0';
+	$sarr['data']['15']['list'][0]['type'] = 'radio';
+	$sarr['data']['15']['list'][1]['type'] = 'radio';
+	$sarr['data']['15']['list'][2]['type'] = 'radio';
+
+	$sarr['data']['16']['title'] = '我宁愿住在僻静的郊区，也不愿住在嘈杂的市区：';
+	$sarr['data']['16']['list'][0]['question'] = '不是的';
+	$sarr['data']['16']['list'][1]['question'] = '不太确定';
+	$sarr['data']['16']['list'][2]['question'] = '是的';
+	$sarr['data']['16']['list'][0]['score'] = '5';
+	$sarr['data']['16']['list'][1]['score'] = '2';
+	$sarr['data']['16']['list'][2]['score'] = '0';
+	$sarr['data']['16']['list'][0]['type'] = 'radio';
+	$sarr['data']['16']['list'][1]['type'] = 'radio';
+	$sarr['data']['16']['list'][2]['type'] = 'radio';
+
+	$sarr['data']['17']['title'] = '我被朋友、同事起过绰号、讥讽过：';
+	$sarr['data']['17']['list'][0]['question'] = '从来没有';
+	$sarr['data']['17']['list'][1]['question'] = '偶尔有过';
+	$sarr['data']['17']['list'][2]['question'] = '这是常有的事';
+	$sarr['data']['17']['list'][0]['score'] = '5';
+	$sarr['data']['17']['list'][1]['score'] = '2';
+	$sarr['data']['17']['list'][2]['score'] = '0';
+	$sarr['data']['17']['list'][0]['type'] = 'radio';
+	$sarr['data']['17']['list'][1]['type'] = 'radio';
+	$sarr['data']['17']['list'][2]['type'] = 'radio';
+
+	$sarr['data']['18']['title'] = '有一种食物使我吃后呕吐：';
+	$sarr['data']['18']['list'][0]['question'] = '没有';
+	$sarr['data']['18']['list'][1]['question'] = '记不清';
+	$sarr['data']['18']['list'][2]['question'] = '有';
+	$sarr['data']['18']['list'][0]['score'] = '5';
+	$sarr['data']['18']['list'][1]['score'] = '2';
+	$sarr['data']['18']['list'][2]['score'] = '0';
+	$sarr['data']['18']['list'][0]['type'] = 'radio';
+	$sarr['data']['18']['list'][1]['type'] = 'radio';
+	$sarr['data']['18']['list'][2]['type'] = 'radio';
+
+	$sarr['data']['19']['title'] = '除去看见的世界外，我的心中没有另外的世界：';
+	$sarr['data']['19']['list'][0]['question'] = '没有';
+	$sarr['data']['19']['list'][1]['question'] = '记不清';
+	$sarr['data']['19']['list'][2]['question'] = '有';
+	$sarr['data']['19']['list'][0]['score'] = '5';
+	$sarr['data']['19']['list'][1]['score'] = '2';
+	$sarr['data']['19']['list'][2]['score'] = '0';
+	$sarr['data']['19']['list'][0]['type'] = 'radio';
+	$sarr['data']['19']['list'][1]['type'] = 'radio';
+	$sarr['data']['19']['list'][2]['type'] = 'radio';
+
+	$sarr['data']['20']['title'] = '我会想到若干年后有什么使自己极为不安的事：';
+	$sarr['data']['20']['list'][0]['question'] = '从来没有想过';
+	$sarr['data']['20']['list'][1]['question'] = '偶尔想到过';
+	$sarr['data']['20']['list'][2]['question'] = '经常想到';
+	$sarr['data']['20']['list'][0]['score'] = '5';
+	$sarr['data']['20']['list'][1]['score'] = '2';
+	$sarr['data']['20']['list'][2]['score'] = '0';
+	$sarr['data']['20']['list'][0]['type'] = 'radio';
+	$sarr['data']['20']['list'][1]['type'] = 'radio';
+	$sarr['data']['20']['list'][2]['type'] = 'radio';
+
+	$sarr['data']['21']['title'] = '我常常觉得自己的家庭对自己不好，但是我又确切地认识他们的确对我好：';
+	$sarr['data']['21']['list'][0]['question'] = '否';
+	$sarr['data']['21']['list'][1]['question'] = '说不清楚';
+	$sarr['data']['21']['list'][2]['question'] = '是';
+	$sarr['data']['21']['list'][0]['score'] = '5';
+	$sarr['data']['21']['list'][1]['score'] = '2';
+	$sarr['data']['21']['list'][2]['score'] = '0';
+	$sarr['data']['21']['list'][0]['type'] = 'radio';
+	$sarr['data']['21']['list'][1]['type'] = 'radio';
+	$sarr['data']['21']['list'][2]['type'] = 'radio';
+
+	$sarr['data']['22']['title'] = '天天我一回家就马上把门关上：';
+	$sarr['data']['22']['list'][0]['question'] = '否';
+	$sarr['data']['22']['list'][1]['question'] = '不清楚';
+	$sarr['data']['22']['list'][2]['question'] = '是';
+	$sarr['data']['22']['list'][0]['score'] = '5';
+	$sarr['data']['22']['list'][1]['score'] = '2';
+	$sarr['data']['22']['list'][2]['score'] = '0';
+	$sarr['data']['22']['list'][0]['type'] = 'radio';
+	$sarr['data']['22']['list'][1]['type'] = 'radio';
+	$sarr['data']['22']['list'][2]['type'] = 'radio';
+
+	$sarr['data']['23']['title'] = '我坐在小房间里把门关上，但我仍觉得心里不安：';
+	$sarr['data']['23']['list'][0]['question'] = '否';
+	$sarr['data']['23']['list'][1]['question'] = '偶尔是';
+	$sarr['data']['23']['list'][2]['question'] = '是';
+	$sarr['data']['23']['list'][0]['score'] = '5';
+	$sarr['data']['23']['list'][1]['score'] = '2';
+	$sarr['data']['23']['list'][2]['score'] = '0';
+	$sarr['data']['23']['list'][0]['type'] = 'radio';
+	$sarr['data']['23']['list'][1]['type'] = 'radio';
+	$sarr['data']['23']['list'][2]['type'] = 'radio';
+
+	$sarr['data']['24']['title'] = '当一件事需要我作决定时，我常觉得很难：';
+	$sarr['data']['24']['list'][0]['question'] = '否';
+	$sarr['data']['24']['list'][1]['question'] = '偶尔是';
+	$sarr['data']['24']['list'][2]['question'] = '是';
+	$sarr['data']['24']['list'][0]['score'] = '5';
+	$sarr['data']['24']['list'][1]['score'] = '2';
+	$sarr['data']['24']['list'][2]['score'] = '0';
+	$sarr['data']['24']['list'][0]['type'] = 'radio';
+	$sarr['data']['24']['list'][1]['type'] = 'radio';
+	$sarr['data']['24']['list'][2]['type'] = 'radio';
+
+	$sarr['data']['25']['title'] = '我常常用抛硬币、翻纸、抽签之类的游戏来猜测凶吉：';
+	$sarr['data']['25']['list'][0]['question'] = '否';
+	$sarr['data']['25']['list'][1]['question'] = '偶尔是';
+	$sarr['data']['25']['list'][2]['question'] = '是';
+	$sarr['data']['25']['list'][0]['score'] = '5';
+	$sarr['data']['25']['list'][1]['score'] = '2';
+	$sarr['data']['25']['list'][2]['score'] = '0';
+	$sarr['data']['25']['list'][0]['type'] = 'radio';
+	$sarr['data']['25']['list'][1]['type'] = 'radio';
+	$sarr['data']['25']['list'][2]['type'] = 'radio';
+
+	$sarr['data']['26']['title'] = '为了工作我早出晚归，早晨起床我常常感到疲劳不堪：';
+	$sarr['data']['26']['list'][0]['question'] = '是';
+	$sarr['data']['26']['list'][1]['question'] = '否';
+	$sarr['data']['26']['list'][0]['score'] = '0';
+	$sarr['data']['26']['list'][1]['score'] = '5';
+	$sarr['data']['26']['list'][0]['type'] = 'radio';
+	$sarr['data']['26']['list'][1]['type'] = 'radio';
+
+	$sarr['data']['27']['title'] = '在某种心境下我会因为困惑陷入空想将工作搁置下来：';
+	$sarr['data']['27']['list'][0]['question'] = '是';
+	$sarr['data']['27']['list'][1]['question'] = '否';
+	$sarr['data']['27']['list'][0]['score'] = '0';
+	$sarr['data']['27']['list'][1]['score'] = '5';
+	$sarr['data']['27']['list'][0]['type'] = 'radio';
+	$sarr['data']['27']['list'][1]['type'] = 'radio';
+
+	$sarr['data']['28']['title'] = '我的神经脆弱稍有刺激就会使我战栗：';
+	$sarr['data']['28']['list'][0]['question'] = '是';
+	$sarr['data']['28']['list'][1]['question'] = '否';
+	$sarr['data']['28']['list'][0]['score'] = '0';
+	$sarr['data']['28']['list'][1]['score'] = '5';
+	$sarr['data']['28']['list'][0]['type'] = 'radio';
+	$sarr['data']['28']['list'][1]['type'] = 'radio';
+
+	$sarr['data']['29']['title'] = '睡梦中我常常被噩梦惊醒：';
+	$sarr['data']['29']['list'][0]['question'] = '是';
+	$sarr['data']['29']['list'][1]['question'] = '否';
+	$sarr['data']['29']['list'][0]['score'] = '0';
+	$sarr['data']['29']['list'][1]['score'] = '5';
+	$sarr['data']['29']['list'][0]['type'] = 'radio';
+	$sarr['data']['29']['list'][1]['type'] = 'radio';
+
+	$sarr['data']['30']['title'] = '工作中我愿意挑战艰巨的任务：';
+	$sarr['data']['30']['list'][0]['question'] = '从不';
+	$sarr['data']['30']['list'][1]['question'] = '几乎不';
+	$sarr['data']['30']['list'][2]['question'] = '一半时间';
+	$sarr['data']['30']['list'][3]['question'] = '大多数时间';
+	$sarr['data']['30']['list'][4]['question'] = '总是';
+	$sarr['data']['30']['list'][0]['score'] = '0';
+	$sarr['data']['30']['list'][1]['score'] = '1';
+	$sarr['data']['30']['list'][2]['score'] = '2';
+	$sarr['data']['30']['list'][3]['score'] = '3';
+	$sarr['data']['30']['list'][4]['score'] = '4';
+	$sarr['data']['30']['list'][0]['type'] = 'radio';
+	$sarr['data']['30']['list'][1]['type'] = 'radio';
+	$sarr['data']['30']['list'][2]['type'] = 'radio';
+	$sarr['data']['30']['list'][3]['type'] = 'radio';
+	$sarr['data']['30']['list'][4]['type'] = 'radio';
+
+
+		
+
+	 	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    $a = json_encode($sarr);
+    exit($a);
+?>
